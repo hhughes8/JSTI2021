48e513b6a441ae560b63c8bee6d3f78c92e62cbb
User <potatoman123567@gmail.com>
2021-07-12 14:20:40 -0600

    animal legs

---
+++ Adam/animallegs.py
@@ -0,0 +1,24 @@
+print("Hey you")
+print("Yeah you")
+print("I need to know some very vital information")
+print("Your input could save the universe")
+print("Pay very close attention to what I'm about to ask you")
+print("How many chickens are there?")
+example = int(input())
+chickenlegs = example * 2
+print(chickenlegs)
+
+print("How many cows are there?")
+exampletoo = int(input())
+cowlegs = exampletoo * 4
+print(cowlegs)
+
+print("How many horses are there?")
+exampletree = int(input())
+horselegs = exampletree * 4
+print(horselegs)
+
+print("now i know how many animal legs there are, thanks bub")
+
+totallegs = chickenlegs + cowlegs + horselegs
+print(totallegs)
