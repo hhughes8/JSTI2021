8a2f13644fd2224751061ba1f93d497bbe4927a9
olivia-232 <oliviachen232@gmail.com>
2021-07-12 15:11:27 -0500

    Hello World

---
+++ Olivia/FarmerLegs.py
@@ -0,0 +1,15 @@
+#example = input()
+
+# number of chickens
+chickens = int(input("# of chickens: "))
+
+# number of cows
+cows = int(input("# of cows: "))
+
+# number of horses
+horses = int(input("# of horses: "))
+
+# total number of legs
+legs = chickens*2 + cows*4 + horses*4
+
+print(legs)
